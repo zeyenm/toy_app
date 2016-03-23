d6214e9683d59a3ff56abd62e867f03f7b85a8c5
Maik Zeyen <maik_zeyen@web.de>
2016-03-23 12:06:44 +0100

    Add integration test for signup page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,6 +5,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   test "layout_links" do
     get root_path
     assert_template 'static_pages/home'
@@ -12,5 +16,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+  
+  test "signup_links" do
+    get signup_path
+    assert_template 'users/new'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "title", full_title("Sign up")
   end
 end
